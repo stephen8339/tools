d37ef46388c68e06ef8876d171f53c6a509e1f47
stephen8339 <55537353@qq.com>
2018-03-16 10:58:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ else
     /data/tools/centos6.sh
     echo "127.0.0.1 $1" > /etc/hosts
     cat /etc/sysconfig/network | sed "s/HOSTNAME=\([0-9]\{1,3\}\-\)\{3\}[0-9]\{1,3\}/HOSTNAME=$1/g" -i /etc/sysconfig/network
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
     cp /data/tools/authorized_keys /root/.ssh/
     echo "Port 3321" >> /etc/ssh/sshd_config
     echo "Port 3321" >> /etc/ssh/ssh_config
